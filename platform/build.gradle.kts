b7fca8512a1229a7821eec6b9d771091745ffee5
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-03-24 21:51:46 +0900

    Update build.gradle.kts

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api(platform("io.ktor:ktor-bom:1.4.0"))
     constraints {
         api("junit:junit:4.12")
+        api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+        api("io.reactivex.rxjava3:rxjava:3.0.0")
     }
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    api("io.reactivex.rxjava3:rxjava:3.0.0")
 }

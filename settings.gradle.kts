b59c67e71a49318ae877d0dfbb7c04e5b9c2fe7c
Masatoshi Kubode <masatoshi@wantedly.com>
2021-11-19 20:41:11 +0900

    Rename rootProject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "playground-gradle-bom-dep-manage"
+rootProject.name = "playground-gradle-catalog-dep-manage"
 enableFeaturePreview("VERSION_CATALOGS")
 include("platform")
 include("shared")

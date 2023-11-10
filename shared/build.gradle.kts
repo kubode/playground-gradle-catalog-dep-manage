cb388ceafe4acb194602dc55f05bc3934281196c
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-10 19:07:35 +0900

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.ksp)
     id("com.github.kubode.gradle.plugin")
 }
 
@@ -11,8 +11,6 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                implementation(libs.ktor.client)
-                implementation(libs.bundles.kotlinx.serialization)
             }
         }
         val commonTest by getting {
@@ -20,20 +18,9 @@ kotlin {
                 implementation(libs.bundles.kotlin.test)
             }
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(libs.rxjava3)
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(libs.junit)
-            }
-        }
     }
 }
 
 dependencies {
     commonMainImplementation(platform(libs.kotlin.bom))
-    commonMainImplementation(platform(libs.ktor.bom))
 }
